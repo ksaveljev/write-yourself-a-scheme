9f1a7c0d0a21ab26a072f6dca20e72c33a8cbea1
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-12-29 11:39:06 +0200

    Parse whitespaces preceding our symbols

--- src/Main.hs
+++ src/Main.hs
@@ -4,8 +4,13 @@ import System.Environment
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+-- Parsec already has 'lexeme' parser but we build our own spaces function
+-- for pedagogical purposes
+spaces :: Parser ()
+spaces = skipMany1 space
+
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse (spaces >> symbol) "lisp" input of
                    Left err -> "No match: " ++ show err
                    Right _ -> "Found value"
 
